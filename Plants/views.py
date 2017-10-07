267ae1479175ae0a1bd9ce72a09d414166ff2c03
krishnadey30 <krishnakumar.d16@iiits.in>
2017-10-07 12:40:07 +0530

    finally ho gaya

--- Plants/views.py
+++ Plants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect,render,get_object_or_404
 from django.http import HttpResponse
-from . models import plant,soil,weather_station,water_reservoir
+from . models import plant,soil
 
 def index(request):
  	return  render(request, 'index.html')
@@ -12,7 +12,8 @@ def plants(request):
 	all_plants=plant.objects.all()
 	return render(request,'plants.html',{'all_plants':all_plants})
 def common(request,plant_id):
-	obj=plant.objects.get(id=plant_id)
+	x=plant.objects.get(id=plant_id)
+	obj=soil.objects.filter(plant_key=x)
 	return render(request,'common.html',{'obj':obj})
 def retrieve(request):
 	WaterLevel=request.GET['WaterLevel']
@@ -21,14 +22,8 @@ def retrieve(request):
 	humidity=request.GET['humidity']
 	temperature=request.GET['temperature']
 	rainChances=request.GET['rainChances']
-	station_id=request.GET['station_id']
-	tank_id=request.GET['tank_id']
-	o=get_object_or_404(plant,id=plantID)
+	o=get_object_or_404(plant,id=plantID,temp=temperature,humidity=humidity,rainfall=rainChances,water_level=WaterLevel)
 	s=soil(plant_key=o,moisture=soilMoisture)
 	s.save()
-	st=weather_station(temp=temperature,humidity=humidity,rainfall=rainChances,station_no=station_id,plant_key=o)
-	st.save()
-	wt=water_reservoir(plant_key=o,water_level=WaterLevel,reservoir_no=tank_id)
-	wt.save()
 	return HttpResponse("sensor_values")
 
